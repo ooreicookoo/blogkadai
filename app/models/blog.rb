1eefc1fcc296a8acfb14b50c36f886e65e51a4af
Reiko Enomoto <ooreicookoo@gmail.com>
2020-08-22 14:18:08 +0900

    kadai_1264 done

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,4 @@
 class Blog < ApplicationRecord
-  validates :title,  length:{in: 1..140}
-  validates :content, presence: true
+  validates :title,  length: { in: 1..140 }
+  validates :content, length:{in: 1..140}
 end
